b514a4ecfcf85e03c09e85f6e58d5ca640ea8655
mpereiraesaa <manuelpereiralds@gmail.com>
2022-04-22 17:14:29 -0300

    Initial commit, base structure, initial entry typescript files for the backend code, using CRA for the frontend subproject initialization

---
+++ server/src/app.ts
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+
+// Load ENV vars
+dotenv.config();
+
+const { SESSION_SECRET } = process.env;
+
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({ secret: SESSION_SECRET }));
+
+export default app;
